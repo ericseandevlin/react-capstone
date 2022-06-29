3cf7e2a9d5e6b810c8bf86ed906a95670e6e025a
Eric <ericdevlin@me.com>
2022-06-28 22:23:37 -0400

    Add sign-up-form, form-input, and button components

--- crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,9 @@
-import { useReducer, useState } from 'react';
+import './sign-up-from.styles.scss';
+import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js';
+
+import FormInput from './../form-input/form-input.component';
+import Button from './../button/button.component.jsx';
 
 const defaultFormFields = {
   displayName: '',
@@ -54,39 +58,45 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className='sign-up-container'>
+
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+
+        <FormInput
+          label="Display Name"
           type="text"
           required onChange={handleChange}
           name="displayName"
           value={displayName} />
 
-        <label>Email</label>
-        <input
+        <FormInput
+          label="Email"
           type="email"
           required onChange={handleChange}
           name="email"
           value={email} />
 
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           required onChange={handleChange}
           name="password"
           value={password} />
 
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           required onChange={handleChange}
           name="confirmPassword"
           value={confirmPassword} />
 
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
+
       </form>
+
     </div>
   )
 }
